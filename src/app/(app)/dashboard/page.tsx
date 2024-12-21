4c7c94dbfb6294147fcfce556ea7537297c05a10
DiyanshuTiwaari <sontitiwa569@gmail.com>
2024-12-21 21:28:13 +0530

    frontendchanged

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,11 +1,11 @@
-'use client'
+'use client';
 
 import { acceptingMessageSchema } from "@/app/schemas/acceptingMessage";
 import MessageCard from "@/components/MessageCard";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { Switch } from "@/components/ui/switch";
-import { useToast } from "@/hooks/use-toast"
+import { useToast } from "@/hooks/use-toast";
 import { Message, User } from "@/models/User";
 import { ApiResponse } from "@/types/apiresponse";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -15,177 +15,177 @@ import { useSession } from "next-auth/react";
 import { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 
-const page = () => {
+const Page = () => { // Capitalized "Page" to follow React component conventions
   const [messages, setMessages] = useState<Message[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isSwitchLoading, setIsSwitchLoading] = useState(false);
 
   const { toast } = useToast();
 
-const handleDeleteMessage = (messageId: string) => {
+  const handleDeleteMessage = (messageId: string) => {
     setMessages(messages.filter((message) => message._id !== messageId));
   };
 
-  
   const { data: session } = useSession();
- 
+
   const form = useForm({
-    resolver: zodResolver(acceptingMessageSchema)
+    resolver: zodResolver(acceptingMessageSchema),
   });
-  
+
   const { register, watch, setValue } = form;
-  
-  const acceptMessages = watch('acceptMessages');
+
+  const acceptMessages = watch("acceptMessages");
+
   const fetchAcceptMessage = useCallback(async () => {
     setIsSwitchLoading(true);
-  
+
     try {
-      const response = await axios.get('/api/accept-messages');
-      setValue('acceptMessages', response.data.isAcceptingMessage);
-    }
-    catch (error) {
-        const axiosError = error as AxiosError<ApiResponse>;
-        toast({
-          title: "Error",
-          description: axiosError.response?.data.message || "Failed to fetch message settings",
-          variant: "destructive"
-        });
-      }
-     finally {
+      const response = await axios.get("/api/accept-messages");
+      setValue("acceptMessages", response.data.isAcceptingMessage);
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: "Error",
+        description: axiosError.response?.data.message || "Failed to fetch message settings",
+        variant: "destructive",
+      });
+    } finally {
       setIsSwitchLoading(false);
     }
-  }, [setValue]);
+  }, [setValue, toast]);
 
-  const fetchMessages = useCallback(async (refresh: boolean = false) => {
-    setIsLoading(true);
-    setIsSwitchLoading(false);
-  
-    try {
-      const response = await axios.get<ApiResponse>('/api/get-messages');
-      setMessages(response.data.Messages || []);
-  
-      if (refresh) {
-        toast({
-            title: "messsage fetch successfully",
-            description: "success to fetch message settings",
-            variant: "destructive"
+  const fetchMessages = useCallback(
+    async (refresh: boolean = false) => {
+      setIsLoading(true);
+      setIsSwitchLoading(false);
+
+      try {
+        const response = await axios.get<ApiResponse>("/api/get-messages");
+        setMessages(response.data.Messages || []);
+
+        if (refresh) {
+          toast({
+            title: "Messages fetched successfully",
+            description: "Successfully fetched messages",
+            variant: "default",
           });
-      }
-    } catch (error) {
+        }
+      } catch (error) {
         const axiosError = error as AxiosError<ApiResponse>;
         toast({
           title: "Error",
-          description: axiosError.response?.data.message || "Failed to fetch message settings",
-          variant: "destructive"
+          description: axiosError.response?.data.message || "Failed to fetch messages",
+          variant: "destructive",
         });
-    }  finally {
+      } finally {
         setIsLoading(false);
         setIsSwitchLoading(false);
       }
-    
-  },[setValue,setMessages]);
+    },
+    [setValue, setMessages, toast]
+  );
+
   useEffect(() => {
     if (!session || !session.user) return;
-  
+
     fetchMessages();
     fetchAcceptMessage();
   }, [session, setValue, fetchAcceptMessage, fetchMessages]);
+
   const handleSwitchChange = async () => {
     try {
-      const response = await axios.post<ApiResponse>('/api/accept-messages', {
-        acceptMessages: !acceptMessages
+      const response = await axios.post<ApiResponse>("/api/accept-messages", {
+        acceptMessages: !acceptMessages,
       });
-  
-      setValue('acceptMessages', !acceptMessages);
+
+      setValue("acceptMessages", !acceptMessages);
       toast({
         title: response.data.message,
-        variant: 'default'
+        variant: "default",
       });
     } catch (error) {
-        const axiosError = error as AxiosError<ApiResponse>;
-        toast({
-          title: "Error",
-          description: axiosError.response?.data.message || "Failed to fetch message settings",
-          variant: "destructive"
-        });
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: "Error",
+        description: axiosError.response?.data.message || "Failed to update message settings",
+        variant: "destructive",
+      });
     }
   };
 
   if (!session || !session.user) {
     return <div>Please login</div>;
   }
-  
+
   const { userName } = session?.user as User;
 
-// TODO: do more research
-const baseUrl = `${window.location.protocol}//${window.location.host}`;
+  const baseUrl = `${window.location.protocol}//${window.location.host}`;
+  const profileUrl = `${baseUrl}/u/${userName}`;
 
-const profileUrl = `${baseUrl}/u/${userName}`;
-
-const copyToClipboard = () => {
-  navigator.clipboard.writeText(profileUrl);
-  toast({
-    title: "URL copied",
-    description: "Profile URL has been copied to clipboard"
-  });
-};
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(profileUrl);
+    toast({
+      title: "URL copied",
+      description: "Profile URL has been copied to clipboard",
+    });
+  };
 
   return (
     <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
       <h1 className="text-4xl font-bold mb-4">User Dashboard</h1>
       <div className="mb-4">
-  <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>
-  <div className="flex items-center">
-    <input
-      type="text"
-      value={profileUrl}
-      disabled
-      className="input input-bordered w-full p-2 mr-2"
-    />
-    <Button onClick={copyToClipboard}>Copy</Button>
-  </div>
-</div>
-<div className="mb-4">
-  <Switch
-    {...register('acceptMessages')}
-    checked={acceptMessages}
-    onCheckedChange={handleSwitchChange}
-    disabled={isSwitchLoading}
-  />
-  <span className="ml-2">
-    Accept Messages: {acceptMessages ? 'On' : 'Off'}
-  </span>
-</div>
-<Separator />
-<Button
-  className="mt-4"
-  variant="outline"
-  onClick={(e) => {
-    e.preventDefault();
-    fetchMessages(true);
-  }}
->
-  {isLoading ? (
-    <Loader2 className="h-4 w-4 animate-spin" />
-  ) : (
-    <RefreshCcw className="h-4 w-4" />
-  )}
-</Button>
-<div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
-  {messages.length > 0 ? (
-    messages.map((message, index) => (
-      <MessageCard
-        key={message._id as string}
-        message={message}
-        onMessageDelete={handleDeleteMessage}
-      />
-    ))
-  ) : (
-    <p>No messages to display.</p>
-  )}
-</div>
+        <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>
+        <div className="flex items-center">
+          <input
+            type="text"
+            value={profileUrl}
+            disabled
+            className="input input-bordered w-full p-2 mr-2"
+          />
+          <Button onClick={copyToClipboard}>Copy</Button>
+        </div>
+      </div>
+      <div className="mb-4">
+        <Switch
+          {...register("acceptMessages")}
+          checked={acceptMessages}
+          onCheckedChange={handleSwitchChange}
+          disabled={isSwitchLoading}
+        />
+        <span className="ml-2">
+          Accept Messages: {acceptMessages ? "On" : "Off"}
+        </span>
+      </div>
+      <Separator />
+      <Button
+        className="mt-4"
+        variant="outline"
+        onClick={(e) => {
+          e.preventDefault();
+          fetchMessages(true);
+        }}
+      >
+        {isLoading ? (
+          <Loader2 className="h-4 w-4 animate-spin" />
+        ) : (
+          <RefreshCcw className="h-4 w-4" />
+        )}
+      </Button>
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+        {messages.length > 0 ? (
+          messages.map((message) => (
+            <MessageCard
+              key={message._id as string}
+              message={message}
+              onMessageDelete={handleDeleteMessage}
+            />
+          ))
+        ) : (
+          <p>No messages to display.</p>
+        )}
+      </div>
     </div>
   );
-
 };
-export default page;
+
+export default Page;
--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,134 +1,121 @@
-"use client"
-import { useForm ,FormProvider} from "react-hook-form";
+"use client";
+
+import { useForm, FormProvider } from "react-hook-form";
 import { signIn } from "next-auth/react";
 import * as z from "zod";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-
-import { useToast } from "@/hooks/use-toast"
 import { useRouter } from "next/navigation";
+import { useToast } from "@/hooks/use-toast";
 import { signinSchema } from "@/app/schemas/signinSchema";
-import axios, { AxiosError } from "axios";
-import { ApiResponse } from "@/types/apiresponse";
-import { zodResolver } from '@hookform/resolvers/zod'
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Loader2 } from "lucide-react";
-const page = () => {
-  
 
-    const { toast } = useToast();
-   
+const Page = () => {
+  const { toast } = useToast();
+  const router = useRouter();
 
+  // Zod form validation using react-hook-form
+  const form = useForm<z.infer<typeof signinSchema>>({
+    resolver: zodResolver(signinSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
 
-
-    const router = useRouter();
-
-    // Zod implementation
-    const form = useForm<z.infer<typeof signinSchema>>({
-        resolver: zodResolver(signinSchema),
-        defaultValues: {
-          
-           identifier: '',
-            password: '',
-        },
+  const onSubmit = async (data: z.infer<typeof signinSchema>) => {
+    const result = await signIn("credentials", {
+      redirect: true,
+      identifier: data.identifier,
+      password: data.password,
     });
- 
-    const onSubmit = async (data: z.infer<typeof signinSchema>) => {
-        
-            const result = await signIn('credentials', {
-              redirect: true,
-              identifier: data.identifier,
-              password: data.password,
-            });
-            if(result?.error){
-              if(result.error == "CredentialsSignin"){
-                toast({
-                    title:"login failed",
-                    description:"incorrect username or password",
-                    variant:"destructive"
-                })
-              }
-              else {
-                toast({
-                    title:"error",
-                    description:result.error,
-                    variant:"destructive"
-                })
-              }
-            }
-            if(result?.url){
-              router.replace("/Dashboard");
-            }
-           
-    };
-    return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-100">
-            <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-                <div className="text-center">
-                    <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-                        Join Mystery Message
-                    </h1>
-                    <p className="mb-4">Sign in to start your anonymous adventure</p>
-                </div>
- <FormProvider {...form}>              
- <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-   
-      <FormField
-      control={form.control}
-      name="identifier"
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>email/username</FormLabel>
-          <FormControl>
-            <Input placeholder="email/username" {...field} 
-                
-                />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-       <FormField
-      control={form.control}
-      name="password"
-      render={({ field }) => (
-        <FormItem>
-          <FormLabel>password</FormLabel>
-          <FormControl>
-            <Input placeholder="password" {...field} 
-                
-                />
-          </FormControl>
-          <FormMessage />
-        </FormItem>
-      )}
-    />
-    <Button type="submit" >
-  signin
-</Button>
-  </form>
-  </FormProvider>
-<div className="text-center mt-4">
-  <p>
-    Already a member?{' '}
-    <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
-      Sign up
-    </Link>
-  </p>
-</div>
 
- </div>
- </div>
-    );
+    if (result?.error) {
+      if (result.error === "CredentialsSignin") {
+        toast({
+          title: "Login failed",
+          description: "Incorrect username or password",
+          variant: "destructive",
+        });
+      } else {
+        toast({
+          title: "Error",
+          description: result.error,
+          variant: "destructive",
+        });
+      }
+    }
 
+    if (result?.url) {
+      router.replace("/Dashboard");
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Mystery Message
+          </h1>
+          <p className="mb-4">Sign in to start your anonymous adventure</p>
+        </div>
+
+        <FormProvider {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="identifier"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email/username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="password" type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit">Sign in</Button>
+          </form>
+        </FormProvider>
+
+        <div className="text-center mt-4">
+          <p>
+            Not a member yet?{" "}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
-export default page
+
+export default Page;
